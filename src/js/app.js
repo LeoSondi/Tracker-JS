81e2d0f00b433a302d763aede98a0ef613a3da3e
LeoSondi <vrnvskdnl@gmail.com>
2024-03-05 17:44:16 +0600

    Начинаю проект

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,17 @@
 
 const input = document.querySelector(".first__input");
 const panel = document.querySelector(".first__panel");
-const notification = document.querySelector(".notification");
+// const notification = document.querySelector(".notification");
 const modal = document.querySelector(".modal");
 const modalContainer = document.querySelector(".modal-container");
 
+// практикую добавление html на лету
+const modalImage = document.createElement("img");
+modalImage.classList.add("modal__img");
+modalImage.setAttribute("src", "src/assets/arrogant.png");
+modal.appendChild(modalImage);
+
+// изменения текста в панели и отображения уведомления
 const changePush = () => {
   if (!input) {
     return;
@@ -13,31 +20,24 @@ const changePush = () => {
   if (input.value != "") {
     panel.innerText = input.value;
   }
+  // сохраняю данные в local storage
+  const data = {
+    text: input.value,
+  };
+  localStorage.setItem("panelText", JSON.stringify(data));
   input.value = "";
+  // **TO-DO** добавить автоматическое скрытие уведомления через 3 секунды
   // notification.classList.add("active");
 };
 
+// Изменение текста по нажатию энтера
 const inputKeydownPush = (e) => {
   if (e.code == "Enter") {
     changePush();
   }
 };
 
-// практикую добавление html на лету
-// добавляю контент в модалку
-const modalImage = document.createElement("img");
-const modalCloseBtn = document.createElement("button");
-
-modalImage.classList.add("modal__img");
-modalCloseBtn.classList.add("close-button");
-
-modalImage.setAttribute("src", "src/assets/arrogant.png");
-modalCloseBtn.innerHTML = `<img src="src/assets/close.svg" width="24px" height="24px"/>`;
-modalCloseBtn.setAttribute("onclick", "closeModal()");
-
-modal.appendChild(modalImage);
-modal.appendChild(modalCloseBtn);
-
+// Открытие и закрытие модалки
 const openModal = () => {
   modalContainer.classList.add("active");
 };
@@ -46,6 +46,7 @@ const closeModal = () => {
   modalContainer.classList.remove("active");
 };
 
+// Закрытие модалки по нажатию эскейпа
 const closeKeydownModal = (e) => {
   if (e.key == "Escape") {
     closeModal();
