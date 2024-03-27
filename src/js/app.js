38a991a142d380b68944490c7943d1933f5c7346
LeoSondi <vrnvskdnl@gmail.com>
2024-03-27 23:27:18 +0600

    Верстаю проект

--- src/js/app.js
+++ src/js/app.js
@@ -1,41 +1,7 @@
 "use strict";
 
-const input = document.querySelector(".first__input");
-const panel = document.querySelector(".first__panel");
-// const notification = document.querySelector(".notification");
 const modal = document.querySelector(".modal");
 const modalContainer = document.querySelector(".modal-container");
-
-// практикую добавление html на лету
-const modalImage = document.createElement("img");
-modalImage.classList.add("modal__img");
-modalImage.setAttribute("src", "src/assets/arrogant.png");
-modal.appendChild(modalImage);
-
-// изменения текста в панели и отображения уведомления
-const changePush = () => {
-  if (!input) {
-    return;
-  }
-  if (input.value != "") {
-    panel.innerText = input.value;
-  }
-  // сохраняю данные в local storage
-  const data = {
-    text: input.value,
-  };
-  localStorage.setItem("panelText", JSON.stringify(data));
-  input.value = "";
-  // **TO-DO** добавить автоматическое скрытие уведомления через 3 секунды
-  // notification.classList.add("active");
-};
-
-// Изменение текста по нажатию энтера
-const inputKeydownPush = (e) => {
-  if (e.code == "Enter") {
-    changePush();
-  }
-};
 
 // Открытие и закрытие модалки
 const openModal = () => {
